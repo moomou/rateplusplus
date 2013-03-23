2ae041bb06aebba178f862a9fc925af30118dc3a
paul <ppymou@gmail.com>
2013-03-23 18:59:55 +0000

    H

---
+++ staticfiles/script/landing.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(function() {
+    var samples = new App.SummaryCardCollectionView({query:DEFAULT_QUERY});
+
+    App.ShowTrendyLink();
+    
+    $('#searchInput').focus();
+    $('#searchBtn').click(function(e) {
+        $('#searchForm').submit()
+    });
+});
+
