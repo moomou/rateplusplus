03572d7998905123a1f8d17913961ce303e51087
Ubuntu <ubuntu@ip-172-31-10-255.us-west-2.compute.internal>
2013-09-21 21:33:26 +0000

    Go GO GO

--- rateplusplus/settings.py
+++ rateplusplus/settings.py
@@ -19,8 +19,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'searchimagery',                      # Or path to database file if using sqlite3.
-        'USER': 'searchimagery',                      # Not used with sqlite3.
+        'NAME': 'clover',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'hack101',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
