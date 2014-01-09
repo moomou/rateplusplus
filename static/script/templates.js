e3f6bb11fa1bf0afad4374304149e50c77a630fe
paul <ppymou@gmail.com>
2014-01-08 21:23:15 -0800

    x

--- static/script/templates.js
+++ static/script/templates.js
@@ -112,7 +112,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   if (stack1 = helpers.avgScore) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.avgScore); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
-    + "' class='pull-left idial'>\n<span class='pull-left sdial'>"
+    + "' class='pull-left idial span1'>\n<span class='pull-left sdial'>"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.summary)),stack1 == null || stack1 === false ? stack1 : stack1.avgScore)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + " %</span>\n<ul class='pull-right'>\n  <li> <i class='icon-check '></i> "
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.summary)),stack1 == null || stack1 === false ? stack1 : stack1.totalVote)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
