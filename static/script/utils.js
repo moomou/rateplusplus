465e344b807fa0e6cd3078a9ef2d9be2a0a7d222
paul <ppymou@gmail.com>
2014-01-14 21:39:50 -0800

    blah

--- static/script/utils.js
+++ static/script/utils.js
@@ -1,6 +1,4 @@
-/*
-    Support Functions
-*/
+/* Support Functions */
 function guidGenerator() {
     var S4 = function() {
        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
