13939afc7f4d9e5cd54a4fb945962582ac7674c0
paul <ppymou@gmail.com>
2014-01-18 12:41:07 -0800

    ok

--- static/script/templates.js
+++ static/script/templates.js
@@ -223,11 +223,11 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  buffer += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" style=\"width:72px; height:72px;\">\n  <circle cx=\"50%\" cy=\"50%\" r=\"50%\" fill=\"";
+  buffer += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" style=\"width:65px; height:65px;\">\n  <circle cx=\"50%\" cy=\"50%\" r=\"50%\" fill=\"";
   if (stack1 = helpers.color) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.color); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
-    + "\"></circle>\n  <text x=\"50%\" dy=\"50%\" text-anchor=\"middle\" dominant-baseline=\"central\" fill=\"white\">";
+    + "\"></circle>\n  <text font-size=\"36px\" x=\"50%\" dy=\"50%\" text-anchor=\"middle\" dominant-baseline=\"central\" fill=\"white\">";
   if (stack1 = helpers.rank) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = (depth0 && depth0.rank); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
   buffer += escapeExpression(stack1)
